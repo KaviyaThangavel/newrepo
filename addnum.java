cecf98233d09309b78c3887ea6e3d7fa82058b16
anitha <anithav@gmail.com>
2021-10-24 16:41:35 +0530

    Added comment

--- addnum.java
+++ addnum.java
@@ -3,8 +3,9 @@ public class AddTwoNumbers {
    public static void main(String[] args) {
         
       int num1 = 5, num2 = 15, sum;
+     
       sum = num1 + num2;
-
+      // To print sum of numbers
       System.out.println("Sum of these numbers: "+sum);
    }
 }
